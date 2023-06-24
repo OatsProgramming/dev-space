bb72b9a8a06117c2025c7689593d389ffa022d4f
Jack Oats <jackoatmeals@gmail.com>
2023-06-24 16:12:22 -0500

    add: link to /create pg & created the pg

---
+++ app/create/page.tsx
@@ -0,0 +1,19 @@
+import { getServerSession } from "next-auth";
+import { authOptions } from "../api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
+import dynamic from "next/dynamic";
+
+// const MarkdownEditor = dynamic(() => 
+//     import('../components/MarkdownEditor/MarkdownEditor')
+// )
+
+export default async function Page() {
+    const session = await getServerSession(authOptions)
+    if (!session) redirect('/signin')
+
+    return (
+        <div>
+            {/* <MarkdownEditor /> */}
+        </div>
+    )
+}
