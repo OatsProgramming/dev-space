3a4762d8e6263ef7aae793c1997273338a622f83
Jack Oats <jackoatmeals@gmail.com>
2023-06-24 22:48:47 -0500

    update: switched out theme switcher with own

--- app/components/Nav/Nav.tsx
+++ app/components/Nav/Nav.tsx
@@ -2,16 +2,16 @@ import { authOptions } from "@/app/api/auth/[...nextauth]/route"
 import { getServerSession } from "next-auth"
 import styles from './nav.module.css'
 // import SearchDialog from "./SearchDialog/SearchDialog"
-// import Notif from "./Notif/Notif"
 // import UserDialog from "./UserDialog/UserDialog"
 // import CreatePost from "./CreatePost/CreatePost"
 import Link from "next/link"
 import Hamburger from "./Hamburger/Hamburger"
-import ThemeSwitch from "../NextUI/ThemeSwitch"
+import ThemeSwitch from "../ThemeSwitch/ThemeSwitch"
+// import Notif from "./Notif/Notif"
 
 export default async function Nav() {
     const session = await getServerSession(authOptions)
-    
+
     return (
         <nav className={styles['nav']}>
             <div className={styles['leftContainer']}>
