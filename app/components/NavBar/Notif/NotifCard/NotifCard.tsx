5163766acfcc219555dc5d493f4c3cb5bf45368a
Jack Oats <jackoatmeals@gmail.com>
2023-06-25 13:35:36 -0500

    add: notif card comp

---
+++ app/components/NavBar/Notif/NotifCard/NotifCard.tsx
@@ -0,0 +1,18 @@
+import styles from './notifCard.module.css'
+import formatDistanceToNow from 'date-fns/formatDistanceToNow'
+
+export default function NotifCard({ notif }: {
+    notif: NotifProps
+}) {
+    const { title, body, createdAt } = notif
+    const timeDiff = formatDistanceToNow(new Date(createdAt), { includeSeconds: true, addSuffix: true })
+    return (
+        <div className={styles['container']}>
+            <div className={styles['text']}>
+                <span>{title}: </span>
+                <span>{body}</span>
+            </div>
+            <p>{timeDiff}</p>
+        </div>
+    )
+}
