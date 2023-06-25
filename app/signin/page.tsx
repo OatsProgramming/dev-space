e705f725592ec4be566717d0d72e4959d6834a37
Jack Oats <jackoatmeals@gmail.com>
2023-06-25 00:14:47 -0500

    add: init signin pg

---
+++ app/signin/page.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+export default function Page() {
+    return (
+        <section>
+            <div>
+                Sign in?
+            </div>
+            <Link href={'/'}>
+                Home
+            </Link>
+        </section>
+    )
+}
