378fc3ffc5c2af89d594c3b08a1d3cf9cfd34620
Jack Oats <jackoatmeals@gmail.com>
2023-06-27 14:22:52 -0500

    add: init design of home pg

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,36 @@
 import postEx from '@/lib/toyData/postEx'
 import PostCard from './components/PostCard/PostCard'
 import styles from './page.module.css'
+import RandomUsers from './components/RandomUsers/RandomUser'
 
 export default function Home() {
   return (
     <main className={styles['main']}>
-      {postEx.map((post, idx) => (
-        <PostCard post={post} key={idx} />
-      ))}
+      <div className={styles['left']}>
+        <div>
+          <h1>Your Top Posts:</h1>
+          <div>Create Post?</div>
+        </div>
+        <div className={styles['posts']}>
+          {postEx.map((post, idx) => (
+            <PostCard post={post} key={idx} isSimple/>
+          ))}
+        </div>
+      </div>
+      <div className={styles['right']}>
+        <div className={styles['posts']}>
+          {postEx.map((post, idx) => (
+            <PostCard post={post} key={idx} />
+          ))}
+          {postEx.map((post, idx) => (
+            <PostCard post={post} key={idx} />
+          ))}
+        </div>
+        <div>
+          <h1>Random Users: </h1>
+          <RandomUsers />
+        </div>
+      </div>
     </main>
   )
 }
