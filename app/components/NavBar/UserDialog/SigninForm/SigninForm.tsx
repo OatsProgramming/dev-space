1689cf3e169978941b77962d88a1ab30ab076cdd
Jack Oats <jackoatmeals@gmail.com>
2023-06-27 14:34:13 -0500

    add: forms for user signin / create acct (init)

---
+++ app/components/NavBar/UserDialog/SigninForm/SigninForm.tsx
@@ -0,0 +1,66 @@
+'use client'
+
+import toggleDialog from '@/lib/toggleDialog';
+import styles from './signinForm.module.css'
+import type { RefObject } from "react";
+import { useState } from 'react'
+import signInUser from './signInUser'
+
+export default function SigninForm({ dialogRef }: {
+    dialogRef: RefObject<HTMLDialogElement>
+}) {
+    const [isNew, setIsNew] = useState(false)
+
+    return (
+        <div className={styles['container']}>
+            <div className={styles['details']}>
+                <span className={styles['textOne']}>
+                    <h1>What's stored?</h1>
+                    <ul>
+                        <li>Username</li>
+                        <li>Password</li>
+                        <li>And your favorited animes</li>
+                    </ul>
+                </span>
+                <span className={styles['textTwo']}>
+                    <div>No cookies. No tracking. Just anime.</div>
+                    <div><i>For privacy, please don't use your email as a username. But hey, I'm not your dad.</i></div>
+                </span>
+            </div>
+            <form onSubmit={(e) => signInUser(e, isNew)} className={styles['form']}>
+                <img
+                    loading='lazy'
+                    src='https://i.imgur.com/8afeTcs.png'
+                    alt='Suika (Dr. Stone) eating a watermelon (Fanart)'
+                />
+                <div className={styles['textField']}>
+                    <input type="text" name='username' placeholder="" required />
+                    <label htmlFor="username">Username</label>
+                </div>
+                <div className={styles['textField']}>
+                    <input type="password" name='password' placeholder="" required />
+                    <label htmlFor="password">Password</label>
+                </div>
+                {isNew && (
+                    <div className={styles['textField']}>
+                        <input type="password" name='confirm' placeholder="" required />
+                        <label htmlFor="confirm">Confirm Password</label>
+                    </div>
+                )}
+                <div className={styles['btns']}>
+                    {/* Mouse event to prevent premature clicks on other elements (for mobile) */}
+                    {/* @ts-expect-error */}
+                    <button onClick={(e) => toggleDialog(e, dialogRef)}>
+                        Cancel
+                    </button>
+                    <button>
+                        {isNew ? 'Create Account' : 'Sign In'}
+                    </button>
+                </div>
+                <button onPointerDown={() => setIsNew(!isNew)}>
+                    {isNew ? 'Have an acccount?' : 'New here?'}
+                </button>
+            </form>
+        </div>
+    )
+}
---
+++ app/components/NavBar/UserDialog/SigninForm/signInUser.ts
@@ -0,0 +1,70 @@
+import mutateFetch from "@/lib/fetchers/mutateFetch"
+import notify from "@/lib/toast/toast"
+import { signIn } from "next-auth/react"
+import type { FormEvent } from "react"
+
+export default async function signInUser(e: FormEvent, isNew: boolean) {
+    e.preventDefault()
+    const form = e.target as HTMLFormElement
+    const formData = new FormData(form)
+    const data = {
+        username: formData.get('username'),
+        password: formData.get('password'),
+    } as UserProps
+
+    if (isNew) {
+        // POST
+        const confirmPassword = formData.get('confirm')
+        if (data.password !== confirmPassword) {
+            return notify({
+                type: 'error',
+                message: `The passwords don't match...`
+            })
+        }
+
+        try {
+            // Create user
+            const res = await mutateFetch('/api/user', 'POST', data)
+
+            if ('error' in res) {
+                let message = 'Server Error'
+                if (res.error.includes('Unique constraint')) {
+                    message = 'User already exists'
+                }
+                return notify({
+                    type: 'error',
+                    message
+                })
+            }
+
+            notify({
+                type: 'success',
+                message: 'User created'
+            })
+        } catch (err) {
+            console.error(err)
+            return
+        }
+    }
+    
+    // Let user know sign in executing
+    notify({
+        type: 'info',
+        message: 'Signing in...'
+    })
+
+    const res = await signIn('credentials', {
+        redirect: false,
+        username: data.username,
+        password: data.password
+    })
+
+    // res.ok will always be true for next-auth
+    if (res?.error) {
+        console.log(res.error)
+        notify({ type: 'error', message: 'Incorrect username / password'})
+    }
+
+    // Manually reload if successful
+    else window.location.reload()
+}
