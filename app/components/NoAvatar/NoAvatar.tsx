e777f638e096132a9fa94c9d01388fb332d3f887
Jack Oats <jackoatmeals@gmail.com>
2023-06-26 21:11:03 -0500

    add: link to associated user's profile

--- app/components/NoAvatar/NoAvatar.tsx
+++ app/components/NoAvatar/NoAvatar.tsx
@@ -1,17 +1,21 @@
 import type { CSSProperties } from "react";
 import styles from './noAvatar.module.css'
+import Link from "next/link";
 
-export default function NoAvatar({ letter }: {
-    letter: string
+export default function NoAvatar({ username }: {
+    username: string
 }) {
     const randomColor = Math.floor(Math.random()*16777215).toString(16)
+    const firstLetter = username.slice(0, 1)
 
     return (
-        <div 
-            className={styles['avatar']} 
-            style={{ '--randomColor': `#${randomColor}` } as CSSProperties}
-        >
-            {letter}
-        </div>
+        <Link href={`/profile/${username}`}>
+            <div
+                className={styles['avatar']}
+                style={{ '--randomColor': `#${randomColor}` } as CSSProperties}
+            >
+                {firstLetter}
+            </div>
+        </Link>
     )
 }
