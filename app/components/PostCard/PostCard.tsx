0e7bd83c8a7fb5b334efca0374953199fcbdcaca
Jack Oats <jackoatmeals@gmail.com>
2023-06-26 21:12:04 -0500

    add: second design form of PostCard

--- app/components/PostCard/PostCard.tsx
+++ app/components/PostCard/PostCard.tsx
@@ -2,12 +2,14 @@ import formatDistanceToNow from 'date-fns/formatDistanceToNow'
 import styles from './postCard.module.css'
 import type { Post } from '@prisma/client'
 import getReadTime from '@/lib/getReadTime'
+import NoAvatar from '../NoAvatar/NoAvatar'
+import Link from 'next/link'
 
 export default function PostCard({ post, isSimple }: {
     post: Post,
     isSimple?: true
-}){
-    const { title, body, createdAt, updatedAt, createdBy } = post
+}) {
+    const { title, body, createdAt, updatedAt, createdBy, id } = post
     const isUpdated = +createdAt !== +updatedAt
 
     let latest = createdAt
@@ -17,15 +19,40 @@ export default function PostCard({ post, isSimple }: {
     const readTime = getReadTime(body)
 
     return (
-        <div className={styles['container']}>
-            <div className={styles['content']}>
-                <h2>{title}</h2>
-                <div>{body}</div>
-            </div>
-            <div className={styles['metadata']}>
-                <div>Read : {readTime}</div>
-                <div>{createdBy} ・ {timeDiff}</div>
-            </div>
+        // Simpler to just do this instead of creating a grid
+        <div>
+            {!isSimple && (
+                <div className={styles['complexMeta']}>
+                    <NoAvatar username={createdBy} />
+                    <div>
+                        <Link href={`/profile/${createdBy}`}>
+                            {createdBy}
+                        </Link> 
+                        <span>
+                            &nbsp;・ {timeDiff}
+                        </span>
+                    </div>
+                </div>
+            )}
+            <Link href={`/post/${id}`}>
+                <div className={styles[isSimple ? 'container' : 'complexContainer']}>
+                    <div className={styles['content']}>
+                        <div>
+                            <h2>{title}</h2>
+                            <div>{body}</div>
+                        </div>
+                        {!isSimple && <div>IMG</div>}
+                    </div>
+                    {isSimple && (
+                        <div className={styles['metadata']}>
+                            <div>{createdBy} ・ {timeDiff}</div>
+                        </div>
+                    )}
+                    <div className={styles['readTime']}>
+                        Read : {readTime}
+                    </div>
+                </div>
+            </Link>
         </div>
     )
 }
