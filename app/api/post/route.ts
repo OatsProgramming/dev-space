d1244f29267826915be479191334df6f840540d5
Jack Oats <jackoatmeals@gmail.com>
2023-06-29 14:26:31 -0500

    touch up

--- app/api/post/route.ts
+++ app/api/post/route.ts
@@ -1,7 +1,7 @@
 import prismadb from "@/lib/prismadb"
 import { NextResponse } from "next/server"
 import validateReq from "./validateReq"
-import deleteComments from "@/lib/deleteComments"
+import deleteComments from "@/lib/prismaHelpers/deleteComments"
 
 // Think abt this when possible: adding categories to search param
 export async function GET(req: Request) {
@@ -9,9 +9,9 @@ export async function GET(req: Request) {
         const { searchParams } = new URL(req.url)
         const postId = searchParams.get('postId')
         if (!postId) return new Response("Post ID not given (/api/post)", { status: 422 })
-    
+
         const post = await prismadb.post.findFirst({
-            where: { id: postId }, 
+            where: { id: postId },
             include: {
                 user: {
                     select: {
@@ -22,9 +22,9 @@ export async function GET(req: Request) {
                 }
             },
         })
-        
+
         if (!post) return new Response("Post not found (/api/post)", { status: 404 })
-    
+
         return NextResponse.json(post)
     } catch (err) {
         return NextResponse.json(err, { status: 500 })
@@ -35,9 +35,9 @@ export async function POST(req: Request) {
     try {
         const res = await validateReq(req)
         if (res instanceof Response) return res
-    
+
         const { method, data } = res
-        switch(method) {
+        switch (method) {
             case 'DELETE': {
                 const { postId } = data
 
@@ -74,7 +74,7 @@ export async function POST(req: Request) {
                     data: {
                         title: data.title,
                         body: data.body,
-                        userId: data.userId!                    
+                        userId: data.userId!
                     }
                 })
                 break;
@@ -88,9 +88,9 @@ export async function POST(req: Request) {
         if ('userId' in data) delete data.userId
 
         return NextResponse.json(data)
-    } catch(err) {
+    } catch (err) {
         console.log(err)
         return NextResponse.json(err, { status: 500 })
     }
-    
+
 }
