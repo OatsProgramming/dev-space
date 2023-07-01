6988fddb81941c1f62a92e74bd3484cc7abc8f9c
Jack Oats <jackoatmeals@gmail.com>
2023-07-01 18:14:30 -0500

    add: cancelable useSWR

---
+++ lib/hooks/useCancelableSWR.ts
@@ -0,0 +1,11 @@
+import type { SWRConfiguration } from "swr"
+import useSWR from "swr"
+import fetcher from "../fetchers/fetcher"
+
+export default function useCancelableSWR (url: string, configs?: SWRConfiguration) {
+    const controller = new AbortController()
+    return {
+        SWRResponse: useSWR(url, (url) => fetcher(url, { signal: controller.signal }), configs), 
+        controller
+    }
+}
