2e916a90e00d8cf7eda185e5aa31aa6fc2004348
Jack Oats <jackoatmeals@gmail.com>
2023-07-01 22:33:09 -0500

    add: nav can now disappear/reappear

--- app/components/NavBar/Nav/Nav.tsx
+++ app/components/NavBar/Nav/Nav.tsx
@@ -1,36 +1,45 @@
 'use client'
 
-import { useRef, useEffect } from "react"
+import { useRef } from "react"
 import styles from './nav.module.css'
+import { useMotionValueEvent, useScroll } from "framer-motion"
 
-// Purely for the blur effect...
 export default function Nav({
   children,
 }: {
   children: React.ReactNode
 }) {
   const navRef = useRef<HTMLElement>(null)
+  const { scrollY } = useScroll()
 
-  useEffect(() => {
-    function handleChange(e: Event) {
-      const nav = navRef.current
-      if (!nav) return 
-      else if (window.scrollY > 0) {
-        nav.style.backdropFilter = 'blur(5px)'
-        // Maybe keep this? To be decided...
-        nav.style.boxShadow = 'var(--subtleGlow)'
-      }
-      else {
-        nav.style.backdropFilter = 'blur(0px)'
-        nav.style.boxShadow = ''
-      }
-    }
+  // Nav disappear (down) / reappear (up)
+  useMotionValueEvent(scrollY, 'velocityChange', (latest) => {
+    const upThreshold = -500
+    const downThreshold = 500
+    const nav = navRef.current
+    if (!nav) return
 
-    window.addEventListener('scroll', handleChange)
-    return () => {
-      window.removeEventListener('scroll', handleChange)
+    else if (latest <= upThreshold) {
+      nav.style.top = "0px"
     }
-  }, [navRef])
+    else if (latest >= downThreshold) {
+      nav.style.top = ""
+    }
+  })
+
+  // Backdrop blur
+  useMotionValueEvent(scrollY, 'change', (latest) => {
+    const nav = navRef.current
+    if (!nav) return
+    else if (latest > 0) {
+      nav.style.backdropFilter = 'blur(15px)'
+      nav.style.boxShadow = 'var(--subtleGlow)'
+    }
+    else {
+      nav.style.backdropFilter = 'blur(0px)'
+      nav.style.boxShadow = ''
+    }
+  })
 
   return (
     <nav ref={navRef} className={styles['nav']}>
