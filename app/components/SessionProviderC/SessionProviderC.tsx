a6f892fb8ced61e016807f3d08cfee375520944d
Jack Oats <jackoatmeals@gmail.com>
2023-07-16 13:41:33 -0500

    placed server session at a context provider in attempt to deal w/ content blocking

--- app/components/SessionProviderC/SessionProviderC.tsx
+++ app/components/SessionProviderC/SessionProviderC.tsx
@@ -1,12 +1,25 @@
 'use client'
+import type { Session } from "next-auth"
 import { SessionProvider } from "next-auth/react";
+import { createContext, useContext } from "react";
 
-export default function SessionProviderC({ children }: {
-    children: React.ReactNode
+const SessionContext = createContext<Session | null>(null)
+
+export default function SessionProviderC({ children, sesh }: {
+    children: React.ReactNode,
+    sesh: Session | null,
 }) {
     return (
+        // This is simply for updating the session if any new info is provided
         <SessionProvider>
-            {children}
+            {/* This is so that the session doesnt get called multiple times ( "cached" essentially ) */}
+            <SessionContext.Provider value={sesh}>
+                {children}
+            </SessionContext.Provider>
         </SessionProvider>
     )
+}
+
+export function getSesh() {
+    return useContext(SessionContext)
 }
