07e6a32ec08e250876f68793bdef0b2578f6a4c0
Jack Oats <jackoatmeals@gmail.com>
2023-07-21 09:23:42 -0500

    add: ProviderComment to mitigate prop drilling

---
+++ app/components/Comments/CommentItem/ProviderComment/ProviderComment.tsx
@@ -0,0 +1,18 @@
+import { createContext, useContext } from "react";
+
+const ContextComment = createContext<CommentResponse | null>(null)
+
+export default function ProviderComment({ comment, children }: {
+    comment: CommentResponse,
+    children: React.ReactNode
+}) {
+    return (
+        <ContextComment.Provider value={comment}>
+            {children}
+        </ContextComment.Provider>
+    )
+}
+
+export function getCommentContext() {
+    return useContext(ContextComment)
+}
