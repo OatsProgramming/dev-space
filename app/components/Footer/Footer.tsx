3df857951a7953ac9e008b95f2adba4223ce4074
Jack Oats <jackoatmeals@gmail.com>
2023-07-25 17:54:06 -0500

    update: UI for Footer

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -2,54 +2,49 @@ import Link from 'next/link'
 import styles from './footer.module.css'
 import logo from '@/public/logo'
 import EmailContact from '../EmailContact/EmailContact'
-import discord from '@/public/discord.svg'
-import github from '@/public/github.svg'
-import Image from 'next/image'
+import github from '@/public/github'
+import discord from '@/public/discord'
+import Icon from '../Icon/Icon'
 
 export default function Footer() {
     return (
-        <footer className={styles['container']}>
+        <footer>
 
-            <div className={styles['flexContainer']}>
-                <div className={styles['webInfo']}>
-                    <Link href='/' className="logo">
+            <div className={styles['container']}>
+                <div className={styles['text']}>
+                    <Link href="/" className='logo'>
                         <img
-                            loading='lazy'
                             src={logo}
-                            alt='logo'
+                            alt='Dev Space logo'
                         />
                     </Link>
-                    <Link href="/">
+                    <h1>
                         Dev Space
-                    </Link>
-                    <div>
-                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel sed tempora dolorem corrupti aliquid dolor nemo laborum reprehenderit deserunt eum. Voluptas, labore aut
+                    </h1>
+                    <div className={styles['details']}>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam, impedit eaque sint quas ea omnis? Asperiores nisi natus fugiat, fugit totam placeat dolores.
+                    </div>
+                    <div className={styles['iconsContainer']}>
+                        <Link href='https://github.com/OatsProgramming/miruTV' target="_blank" rel='noreferrer noopener'>
+                            <Icon
+                                img={github}
+                                alt='Github Icon'
+
+                            />
+                        </Link>
+                        <Link href='https://discord.gg/WUhmy8CaB' target='_blank' rel='noreferrer noopener'>
+                            <Icon
+                                img={discord}
+                                alt='Discord Icon'
+
+                            />
+                        </Link>
                     </div>
                 </div>
 
                 <div className={styles['contact']}>
+                    <h1>Got A Suggestion?</h1>
                     <EmailContact />
-                    <div className={styles['iconsContainer']}>
-                        {/* Note: using <Image /> instead of <img /> since the latter won't work with svgs for some reason... maybe convert it to base64 later */}
-                        <Link href='https://github.com/OatsProgramming/miruTV' target="_blank" rel='noreferrer noopener'>
-                            <Image
-                                loading='lazy'
-                                src={github}
-                                alt='Github Icon'
-                                width={100}
-                                height={100}
-                            />
-                        </Link>
-                        <Link href='https://discord.gg/WUhmy8CaB' target='_blank' rel='noreferrer noopener'>
-                            <Image
-                                loading='lazy'
-                                src={discord}
-                                alt='Discord Icon'
-                                width={100}
-                                height={100}
-                            />
-                        </Link>
-                    </div>
                 </div>
             </div>
 
