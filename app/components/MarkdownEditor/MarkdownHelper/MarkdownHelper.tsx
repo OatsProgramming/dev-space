96022fe876c141a0e4eef92c2dcf82987d0cb701
Jack Oats <jackoatmeals@gmail.com>
2023-07-25 21:22:20 -0500

    integrated UnsplashDialog to MarkdownHelper

--- app/components/MarkdownEditor/MarkdownHelper/MarkdownHelper.tsx
+++ app/components/MarkdownEditor/MarkdownHelper/MarkdownHelper.tsx
@@ -1,11 +1,19 @@
-import { useCallback, type RefObject, useEffect } from "react";
+import { useCallback, type RefObject, useEffect, useState } from "react";
 import useUI from "@/app/components/MarkdownEditor/hooks/useUI";
 import './markdownHelper.css'
+import dynamic from "next/dynamic";
+
+// Dont have a loader for this
+const UnsplashDialog = dynamic(() =>
+    import("../../UnsplashProvider/Dialog/UnsplashDialog")
+)
 
 function MarkdownHelper({ textareaRef }: {
     textareaRef: RefObject<HTMLTextAreaElement>
 }) {
+    const [isOpen, setIsOpen] = useState(false)
     const { selectedText, isDark, isPreview, setIsDark, setIsPreview, setText, setSelectedText } = useUI()
+
     const addHelper = useCallback((helper: MarkdownHelperTypes) => {
         const textarea = textareaRef.current
         if (!textarea) return
@@ -82,73 +90,68 @@ function MarkdownHelper({ textareaRef }: {
         setIsDark(!isDark)
     }
 
-    useEffect(() => {
-        const darkMode = window.matchMedia('(prefers-color-scheme: dark)');
-        function handleTheme() {
-            if (darkMode.matches) {
-                document.body.classList.add('dark')
-                document.body.classList.remove('light')
-            }
-            else {
-                document.body.classList.remove('dark')
-                document.body.classList.add('light')
-            }
-        }
-        darkMode.addEventListener('change', handleTheme)
-        return () => {
-            darkMode.removeEventListener('change', handleTheme)
-        }
-    }, [])
-
     return (
-        <div className="btns">
-            <div className="toggles">
-                <button onPointerDown={toggleTheme}>
-                    Toggle Theme
-                </button>
-                <button className={`${!isPreview && 'clicked'}`} onPointerDown={() => setIsPreview(!isPreview)}>
-                    {isPreview ? 'Edit' : 'Preview'}
-                </button>
-                <button onPointerDown={() => setText("")}>
-                    Clear
-                </button>
+        <>
+            <div className="btns">
+                <div className="toggles">
+                    <button onPointerDown={toggleTheme}>
+                        Toggle Theme
+                    </button>
+                    <button className={`${!isPreview && 'clicked'}`} onPointerDown={() => setIsPreview(!isPreview)}>
+                        {isPreview ? 'Edit' : 'Preview'}
+                    </button>
+                    <button onPointerDown={() => setText("")}>
+                        Clear
+                    </button>
+                </div>
+                <div className="helpers">
+                    <select name="heading_size" onPointerDown={(e) => addHelper((e.target as HTMLSelectElement).value as MarkdownHelperTypes)}>
+                        <option value="heading1">Heading</option>
+                        <option value="heading2">Heading 2</option>
+                        <option value="heading3">Heading 3</option>
+                        <option value="heading4">Heading 4</option>
+                        <option value="heading5">Heading 5</option>
+                        <option value="heading6">Heading 6</option>
+                    </select>
+                    <button onPointerDown={() => addHelper('italic')}>
+                        Italic
+                    </button>
+                    <button onPointerDown={() => addHelper('bold')}>
+                        Bold
+                    </button>
+                    <button onPointerDown={() => addHelper('code')}>
+                        Code
+                    </button>
+                    <button onPointerDown={() => addHelper('codeBlock')}>
+                        Code Block
+                    </button>
+                    <button onPointerDown={() => addHelper('quote')}>
+                        Quote
+                    </button>
+                    <button onPointerDown={() => addHelper('multiQuote')}>
+                        Multi Quote
+                    </button>
+                    <button onPointerDown={() => addHelper('unorderItem')}>
+                        Unordered Item
+                    </button>
+                    <button onPointerDown={() => addHelper('blankLine')}>
+                        Blank line
+                    </button>
+                    {/* This is set to position: fixed... supposed to be relative to viewport but yet its showing otherwise */}
+                    {/* Going to use UnsplashDialog directly to try and circumvent the issue */}
+                    {/* <UnsplashProvider /> */}
+                    <button onClick={() => setIsOpen(true)}>
+                        Find Photos
+                    </button>
+                </div>
             </div>
-            <div className="helpers">
-                {/* @ts-expect-error */}
-                <select name="heading_size" onPointerDown={(e) => addHelper(e.target.value)}>
-                    <option value="heading1">Heading</option>
-                    <option value="heading2">Heading 2</option>
-                    <option value="heading3">Heading 3</option>
-                    <option value="heading4">Heading 4</option>
-                    <option value="heading5">Heading 5</option>
-                    <option value="heading6">Heading 6</option>
-                </select>
-                <button onPointerDown={() => addHelper('italic')}>
-                    Italic
-                </button>
-                <button onPointerDown={() => addHelper('bold')}>
-                    Bold
-                </button>
-                <button onPointerDown={() => addHelper('code')}>
-                    Code
-                </button>
-                <button onPointerDown={() => addHelper('codeBlock')}>
-                    Code Block
-                </button>
-                <button onPointerDown={() => addHelper('quote')}>
-                    Quote
-                </button>
-                <button onPointerDown={() => addHelper('multiQuote')}>
-                    Multi Quote
-                </button>
-                <button onPointerDown={() => addHelper('unorderItem')}>
-                    Unordered Item
-                </button>
-                <button onPointerDown={() => addHelper('blankLine')}>
-                    Unordered Item
-                </button>
-            </div>
-        </div>
+            {isOpen && (
+                <UnsplashDialog
+                    isOpen={isOpen}
+                    setIsOpen={setIsOpen}
+                />
+            )}
+        </>
     )
 }
 
