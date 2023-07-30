ee9eeaca1cb7334e1094d1bb95e5c3d33ebbbc61
Jack Oats <jackoatmeals@gmail.com>
2023-07-30 16:41:43 -0500

    update: UI and logic for MarkdownEditor

--- app/components/MarkdownEditor/MarkdownHelper/MarkdownHelper.tsx
+++ app/components/MarkdownEditor/MarkdownHelper/MarkdownHelper.tsx
@@ -1,33 +1,51 @@
+'use client'
+
 import { useCallback, useState } from "react";
 import useUI from "@/app/components/MarkdownEditor/hooks/useUI";
-import './markdownHelper.css'
 import dynamic from "next/dynamic";
 import { getTextarea } from "../context/TextareaProvider";
+import Dropdown from "../../Dropdown/Dropdown";
+import styles2 from '@/app/post/components/ActionBar/actionBar.module.css'
+import styles from './markdownHelper.module.css'
+
+import AnimationProviderMAX from "../../context/AnimationProvider/AnimationProviderMAX";
+import { m } from "framer-motion";
+import tools from "@/public/tools";
+import Icon from "../../Icon/Icon";
 
 // Dont have a loader for this
 const UnsplashDialog = dynamic(() =>
     import("../../UnsplashDialog/UnsplashDialog")
 )
 
-function MarkdownHelper() {
+export default function MarkdownHelper() {
     const textarea = getTextarea()
+    if (!textarea) return
 
-    const [isOpen, setIsOpen] = useState(false)
+    const [isOpen, setIsOpen] = useState(true)
+    const [isUnsplashOpen, setUnsplashOpen] = useState(false)
     const { selectedText, isPreview, setIsPreview, setText, setSelectedText } = useUI()
 
-    const addHelper = useCallback((helper: MarkdownHelperTypes) => {
-        if (!textarea) return
+    const headingSizes: MarkdownHelperTypes[] = [
+        'heading_1',
+        'heading_2',
+        'heading_3',
+        'heading_4',
+        'heading_5',
+        'heading_6'
+    ]
 
+    const addHelper = useCallback((helper: MarkdownHelperTypes) => {
         const text = selectedText || '_INSERT_TEXT_'
         let helperText: string;
         switch (helper) {
-            case 'heading1':
-            case 'heading2':
-            case 'heading3':
-            case 'heading4':
-            case 'heading5':
-            case 'heading6': {
-                const size = Number(helper.substring(helper.length - 1, helper.length))
+            case 'heading_1':
+            case 'heading_2':
+            case 'heading_3':
+            case 'heading_4':
+            case 'heading_5':
+            case 'heading_6': {
+                const size = Number(helper.charAt(helper.length - 1))
                 const headingSize = "#".repeat(size)
                 helperText = `\n${headingSize} ${text}\n`
                 break;
@@ -79,65 +97,94 @@ function MarkdownHelper() {
         setSelectedText("")
     }, [selectedText])
 
+    // TODO: make own modal for this...?
+    const clearContent = useCallback(() => {
+        const clr = confirm("Are you sure you want to delete the entire thing?")
+        if (clr) setText('')
+    }, [])
+
     return (
-        <>
-            <div className="btns">
-                <div className="toggles">
-                    <button className={`${!isPreview && 'clicked'}`} onPointerDown={() => setIsPreview(!isPreview)}>
-                        {isPreview ? 'Edit' : 'Preview'}
+        <AnimationProviderMAX>
+            <m.div
+                layout
+                layoutDependency={isOpen}
+                className={`
+                    ${styles2['container']}
+                    ${styles['btns']}
+                `}
+            >
+                {isOpen ? (
+                    <>
+                        <div className={styles['toggles']}>
+                            <button className={`${!isPreview && 'clicked'}`} onPointerDown={() => setIsPreview(!isPreview)}>
+                                {isPreview ? 'Edit' : 'Preview'}
+                            </button>
+                            <button onPointerDown={clearContent}>
+                                Clear
+                            </button>
+                            <button onClick={() => setIsOpen(false)}>
+                                Close
+                            </button>
+                        </div>
+                        <div className={styles['helpers']}>
+                            <Dropdown
+                                name="Heading Sizes"
+                                items={headingSizes}
+                                setChange={addHelper}
+                            />
+                            <button onPointerDown={() => addHelper('italic')}>
+                                Italic
+                            </button>
+                            <button onPointerDown={() => addHelper('bold')}>
+                                Bold
+                            </button>
+                            <button onPointerDown={() => addHelper('code')}>
+                                Code
+                            </button>
+                            <button onPointerDown={() => addHelper('codeBlock')}>
+                                Code Block
+                            </button>
+                            <button onPointerDown={() => addHelper('quote')}>
+                                Quote
+                            </button>
+                            <button onPointerDown={() => addHelper('multiQuote')}>
+                                Multi Quote
+                            </button>
+                            <button onPointerDown={() => addHelper('unorderItem')}>
+                                Unordered Item
+                            </button>
+                            <button onPointerDown={() => addHelper('blankLine')}>
+                                Blank line
+                            </button>
+                            {/* This is set to position: fixed... supposed to be relative to viewport but yet its showing otherwise */}
+                            {/* Going to use UnsplashDialog directly to try and circumvent the issue */}
+                            {/* <UnsplashProvider /> */}
+                            <button onClick={() => setUnsplashOpen(true)}>
+                                Find Photos
+                            </button>
+                        </div>
+                    </>
+                ) : (
+                    <button
+                        onClick={() => setIsOpen(true)}
+                        className={styles['icon']}
+                    >
+                        Helpers
+                        <div>
+                            <Icon
+                                img={tools}
+                                alt='Tools icon'
+                            />
+                        </div>
                     </button>
-                    <button onPointerDown={() => setText("")}>
-                        Clear
-                    </button>
-                </div>
-                <div className="helpers">
-                    <select name="heading_size" onPointerDown={(e) => addHelper((e.target as HTMLSelectElement).value as MarkdownHelperTypes)}>
-                        <option value="heading1">Heading</option>
-                        <option value="heading2">Heading 2</option>
-                        <option value="heading3">Heading 3</option>
-                        <option value="heading4">Heading 4</option>
-                        <option value="heading5">Heading 5</option>
-                        <option value="heading6">Heading 6</option>
-                    </select>
-                    <button onPointerDown={() => addHelper('italic')}>
-                        Italic
-                    </button>
-                    <button onPointerDown={() => addHelper('bold')}>
-                        Bold
-                    </button>
-                    <button onPointerDown={() => addHelper('code')}>
-                        Code
-                    </button>
-                    <button onPointerDown={() => addHelper('codeBlock')}>
-                        Code Block
-                    </button>
-                    <button onPointerDown={() => addHelper('quote')}>
-                        Quote
-                    </button>
-                    <button onPointerDown={() => addHelper('multiQuote')}>
-                        Multi Quote
-                    </button>
-                    <button onPointerDown={() => addHelper('unorderItem')}>
-                        Unordered Item
-                    </button>
-                    <button onPointerDown={() => addHelper('blankLine')}>
-                        Blank line
-                    </button>
-                    {/* This is set to position: fixed... supposed to be relative to viewport but yet its showing otherwise */}
-                    {/* Going to use UnsplashDialog directly to try and circumvent the issue */}
-                    {/* <UnsplashProvider /> */}
-                    <button onClick={() => setIsOpen(true)}>
-                        Find Photos
-                    </button>
-                </div>
-            </div>
-            {isOpen && (
+                )}
+            </m.div>
+            {/* This is fine: wont ever open unless isOpen: true first */}
+            {isUnsplashOpen && (
                 <UnsplashDialog
-                    setIsOpen={setIsOpen}
+                    setIsOpen={setUnsplashOpen}
                 />
             )}
-        </>
+        </AnimationProviderMAX>
     )
 }
-
-export default MarkdownHelper
--- app/components/MarkdownEditor/MarkdownHelper/markdownHelperTypes.d.ts
+++ app/components/MarkdownEditor/MarkdownHelper/markdownHelperTypes.d.ts
@@ -1,4 +1,4 @@
 type MarkdownHelperTypes =
-    "heading1" | "heading2" | "heading3" | "heading4" | "heading5" | "heading6"
+    "heading_1" | "heading_2" | "heading_3" | "heading_4" | "heading_5" | "heading_6"
     | "italic" | "bold" | "quote" | "multiQuote" | "code" | "codeBlock" | "unorderItem"
     | "blankLine"
