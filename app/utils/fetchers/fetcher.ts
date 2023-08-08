4c536425a99010efde0a13cc6c145c8fcd7945fa
Jack Oats <jackoatmeals@gmail.com>
2023-08-08 16:35:45 -0500

    add: timeouts for fetcher fns

--- app/utils/fetchers/fetcher.ts
+++ app/utils/fetchers/fetcher.ts
@@ -1,5 +1,15 @@
-export default async function fetcher(url: string, options?: RequestInit) {
+export default async function fetcher(url: string, options?: RequestInit, timeout = 8000) {
+    const controller = new AbortController()
+
+    const timeoutId = setTimeout(() => 
+        controller.abort(),
+        timeout
+    ) 
+
     const res = await fetch(url, options)
+
+    clearTimeout(timeoutId)
+    
     if (!res.ok) {
         const result = await res.text()
         throw new Error(result)
--- app/utils/fetchers/mutateFetch.ts
+++ app/utils/fetchers/mutateFetch.ts
@@ -3,14 +3,25 @@ type MutateFetchReturn<T> = {
 }
 
 export default async function mutateFetch<T>(url: string, method: MutateHTTP, data: T): Promise<MutateFetchReturn<T>> {
+    const timeout = 8_000
+    const controller = new AbortController()
+
+    const timeoutId = setTimeout(() => 
+        controller.abort(),
+        timeout
+    )
+
     const res = await fetch(url, {
         //  Set only to POST here. NextJS still have issues w/ other HTTP methods
         method: 'POST',
         body: JSON.stringify({
             method,
             data: {...data}
-        })
+        }),
+        signal: controller.signal
     })
+
+    clearTimeout(timeoutId)
 
     if (!res.ok) {
         const message = await res.text()
