6d5aeb934f4f819b92eeec9aee1d294cf4d8fa53
Jack Oats <jackoatmeals@gmail.com>
2023-08-09 20:21:02 -0500

    refactored RandomCards

--- app/components/RandomCards/RandomCards.tsx
+++ app/components/RandomCards/RandomCards.tsx
@@ -1,56 +1,36 @@
-import userEx from "@/app/utils/toyData/userEx"
+// import userEx from "@/app/utils/toyData/userEx"
 import UserCard from "../UserCard/UserCard"
 import styles from './randomCards.module.css'
 import fetcher from "@/app/utils/fetchers/fetcher"
 import baseUrl from "@/app/utils/baseUrl"
-import { areComments, arePosts, areUsers } from "./validators"
 import PostCard from "../PostCard/PostCard"
-import Comments from "../Comments/Comments"
+import ProviderComment from "../Comments/CommentItem/ProviderComment/ProviderComment"
+import CommentItem from "../Comments/CommentItem/CommentItem"
 
 export default async function RandomCards({ category }: {
     category: RandomCategoryParam
 }) {
     const items = await fetcher(`${baseUrl}/api/random/${category}`) as RandomCardType[typeof category]
-
-    if (arePosts(items)) {
-        return (
-            <div className={styles['container']}>
-                {items.map((item) => (
-                    <PostCard post={item} key={item.id} isSimple />
-                ))}
-            </div>
-        )
-    }
-
-    else if (areUsers(items)) {
-        return (
-            <div className={styles['container']}>
-                {items.map((item) => (
-                    <UserCard user={item} key={item.id} />
-                ))}
-            </div>
-        )
-    }
-
-    // TODO: Figure out if you're planning to use areComments
-    // else if (areComments(items)) {
-    //     return (
-    //         <div className={styles['container']}>
-    //             {items.map((item, i) => (
-    //                 <Comments user={item} key={i} />
-    //             ))}
-    //         </div>
-    //     )
-    // }
-
     
-    // const items = userEx
-
-    else {
-        return (
-            <div className={styles['container']}>
-                <div>Unknown category</div>
-            </div>
-        )
-    }
+    return (
+        <div className={styles['container']}>
+            {items.map(item => (
+                category === 'posts'
+                    ? <PostCard post={item as PostResponse} key={item.id}/> 
+                    : category === 'users'
+                        ? <UserCard user={item as UserResponse} key={item.id} />
+                        // TODO: Consider whether or not to keep this. 
+                        // Might be a hassle to refactor this for a different UI 
+                        // to click to main post instead of just the replies...
+                        : (
+                            <ProviderComment
+                                comment={item as CommentResponse}
+                                key={item.id}
+                            >
+                                <CommentItem />
+                            </ProviderComment>
+                        )
+            ))}
+        </div>
+    )
 }
