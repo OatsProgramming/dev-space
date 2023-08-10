ff0562b93c5a99d03eda4e9b77d0793e7e9a95cc
Jack Oats <jackoatmeals@gmail.com>
2023-08-09 19:56:09 -0500

    modified UserDialog UI btn

--- app/components/AuthBtns/AuthBtns.tsx
+++ app/components/AuthBtns/AuthBtns.tsx
@@ -3,6 +3,8 @@
 import type { User } from "next-auth"
 import { signIn, signOut, useSession } from "next-auth/react"
 import { useRouter } from "next/navigation"
+import styles2 from '@/app/components/NavBar/UserDialog/userDialog.module.css'
+import notify from "@/lib/toast/toast"
 
 type UpdateArgs = Partial<User>
 export function SignOut() {
@@ -11,7 +13,17 @@ export function SignOut() {
     async function handleSignOut(e: React.PointerEvent) {
         e.preventDefault()
         // Seems that the bugs present w/ auth is fixed with ^13.4
-        await signOut({ redirect: false })
+        // Using a toast since signing out is a bit slow
+        await notify({
+            type: 'promise',
+            promise: signOut({ redirect: false }),
+            messages: {
+                success: "Signed out",
+                error: "Something went wrong...",
+                pending: "Signing out..."
+            }
+        })
+        // Refresh since redirect is false
         router.refresh()
     }
 
@@ -29,13 +41,16 @@ export function SignOut() {
 
 export function SignIn() {
     return (
-        <button onClick={() => signIn()}>
-            Sign in
+        <button className={styles2['btn']} onClick={() => signIn()}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="currentColor" viewBox="0 0 16 16">
+                <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z" />
+            </svg>
+            <div>Sign in</div>
         </button>
     )
 }
 
-// TEMP
+// TODO: Integrate this once "updating user info" is ready
 export function UpdateSeshBtn({ toUpdate }: {
     toUpdate: UpdateArgs,
 }) {
@@ -47,7 +62,7 @@ export function UpdateSeshBtn({ toUpdate }: {
             .then(_ => router.refresh())
             .catch(err => console.log(err))
     }
-    
+
 
     return (
         <button onClick={() => updateSesh(toUpdate)}>
